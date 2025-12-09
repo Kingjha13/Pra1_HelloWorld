a0d3893c20168387e91f0a8f308601350eb44173
Avanish jha <avnishjha277@gmil.com>
2025-12-09 23:21:49 +0530

    Hello world project of college lab

---
+++ app/src/main/java/com/example/hello_world/HelloActivity.kt
@@ -0,0 +1,32 @@
+package com.example.hello_world
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class HelloActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            hello()
+        }
+    }
+}
+@Composable
+fun hello(){
+    Box(modifier= Modifier.padding(top = 40.dp).fillMaxSize().background(color = Color(0xFFBBDEFB))){
+        Text(text="Hello, World", fontSize = 24.sp, modifier=Modifier.padding(20.dp).padding(start = 90.dp, top = 350.dp), color = Color(0xFF0D47A1))
+    }
+}
