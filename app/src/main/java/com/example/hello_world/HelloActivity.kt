f7b8a24df22c6c2530c2935d5c456bc80bcba8ba
Avanish jha <avnishjha277@gmil.com>
2025-12-10 23:54:59 +0530

    I also add java with xml for this practical 1.

--- app/src/main/java/com/example/hello_world/HelloActivity.kt
+++ app/src/main/java/com/example/hello_world/HelloActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hello_world
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,18 +16,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-class HelloActivity : ComponentActivity() {
+class HelloActivity: ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        setContent{
             hello()
         }
     }
 }
 @Composable
 fun hello(){
-    Box(modifier= Modifier.padding(top = 40.dp).fillMaxSize().background(color = Color(0xFFBBDEFB))){
+    Box(modifier= Modifier.fillMaxSize().background(color = Color(0xFFBBDEFB))){
         Text(text="Hello, World", fontSize = 24.sp, modifier=Modifier.padding(20.dp).padding(start = 90.dp, top = 350.dp), color = Color(0xFF0D47A1))
     }
 }
